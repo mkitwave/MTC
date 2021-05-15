14ab09794cd3a9d8b30d3375d1fadf6f7160aef5
맹수연 <sooyeon030908@gmail.com>
2021-05-15 21:56:45 +0900

    210515

---
+++ maeng/210515-1.py
@@ -0,0 +1,9 @@
+# https://programmers.co.kr/learn/courses/4008/lessons/13254
+
+def solution(mylist):
+    result = []
+    for i in range(len(mylist)):
+        length = len(mylist[i])
+        result.append(length)
+    answer = result
+    return answer
