9e45bca850245b82308c748e10cbd7fd53ecf436
맹수연 <sooyeon030908@gmail.com>
2021-05-19 13:03:03 +0900

    complete special_quest

---
+++ Special_Quest/210518-NM(1)/maeng.py
@@ -0,0 +1,16 @@
+if __name__ == '__main__':
+    n, m = map(int, input().split())
+    s = []
+
+    def f():
+        if len(s) == m:
+            print(' '.join(map(str, s)))
+            return
+
+        for i in range(1, n + 1):
+            if i in s:
+                continue
+            s.append(i)
+            f()
+            s.pop()
+    f()
