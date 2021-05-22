425feb890e913134eeb347a0cfffc8ae554bf742
맹수연 <sooyeon030908@gmail.com>
2021-05-22 20:04:16 +0900

    210522

---
+++ maeng/baekjoon/210522/1110번-더하기사이클.py
@@ -0,0 +1,14 @@
+# https://www.acmicpc.net/problem/1110
+
+tmp = N = int(input())
+count = 0
+while True:
+    ten = tmp // 10
+    one = tmp % 10
+    res = ten + one
+    count += 1
+    tmp = int(str(tmp % 10) + str(res % 10))
+
+    if (N == tmp):
+        break
+print(count)
